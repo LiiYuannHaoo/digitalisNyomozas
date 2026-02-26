9ba05271cb749c87bf0c0f823e1aa4bdd3f2ba9c
LiiYuannHaoo <li.yuanhao@petrik.hu>
2026-02-26 22:41:24 +0100

    bizonyitek rogzitese

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,7 +15,7 @@ internal class Program
             Console.WriteLine("1 Új ugy");
             Console.WriteLine("2 Ügyek listázása");
             Console.WriteLine("3 Személy hozzáadás");
-
+            Console.WriteLine("4 Bizonyíték rögzítés");
 
             string valasz = Console.ReadLine() ?? "";
 
@@ -24,7 +24,7 @@ internal class Program
                 case "1":
                     Console.Write("Azonosító: ");
                     string id = Console.ReadLine() ?? "";
-                    Console.Write("Cím: ");
+                    Console.Write("Cim: ");
                     string cim = Console.ReadLine() ?? "";
                     Console.Write("Leírás: ");
                     string leiras = Console.ReadLine() ?? "";
@@ -53,7 +53,26 @@ internal class Program
                     }
                     break;
 
-        
+                case "4":
+                    Console.Write("Ügy ID: ");
+                    string bid = Console.ReadLine() ?? "";
+                    var ugy2 = store.Ugyek.Find(x => x.UgyAzonosito == bid);
+
+                    if (ugy2 != null)
+                    {
+                        Console.Write("Bizonyíték ID: ");
+                        string eid = Console.ReadLine() ?? "";
+                        Console.Write("Tipus: ");
+                        string tipus = Console.ReadLine() ?? "";
+                        Console.Write("Leírás: ");
+                        string l = Console.ReadLine() ?? "";
+                        Console.Write("Megbízhatósag (1-5): ");
+                        int m = int.Parse(Console.ReadLine() ?? "1");
+
+                        ugy2.BizonyitekHozzaadas(new Evidence(eid, tipus, l, m));
+                    }
+                    break;
+
             }
         }
     }
