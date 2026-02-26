716fd2a31b5b75e6bbd507e3cd5fcc184a8bc6ae
LiiYuannHaoo <li.yuanhao@petrik.hu>
2026-02-26 21:05:47 +0100

    uj ugy es ugyek lista

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System;
+using DigitalisNyomozas;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataStore store = new DataStore();
+        DecisionEngine engine = new DecisionEngine();
+
+        bool fut = true;
+
+        while (fut)
+        {
+            Console.WriteLine("1 Új ügy");
+            Console.WriteLine("2 Ügyek listázása");
+
+            string valasz = Console.ReadLine() ?? "";
+
+            switch (valasz)
+            {
+                case "1":
+                    Console.Write("Azonosító: ");
+                    string id = Console.ReadLine() ?? "";
+                    Console.Write("Cím: ");
+                    string cim = Console.ReadLine() ?? "";
+                    Console.Write("Leírás: ");
+                    string leiras = Console.ReadLine() ?? "";
+
+                    store.Ugyek.Add(new Case(id, cim, leiras));
+                    break;
+
+                case "2":
+                    foreach (var u in store.Ugyek)
+                        Console.WriteLine(u);
+                    break;
+
+                
+            }
+        }
+    }
+}
